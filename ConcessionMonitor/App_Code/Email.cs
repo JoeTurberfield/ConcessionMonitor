3d2239f31f4c7e665bb7ea425bbfd562de8aaab0
joe.turberfield <joeturberfield@hotmail.co.uk>
2022-02-27 08:39:03 +0000

    emails

--- ConcessionMonitor/App_Code/Email.cs
+++ ConcessionMonitor/App_Code/Email.cs
@@ -42,15 +42,15 @@ namespace ConcessionMonitor
                         // Send to product groups by machine type.
                         if(machType == "VCN430A/530C" || machType == "VTC530C/760C")
                         {
-                            message.CC.Add("tperrin@mazak.co.uk");
+                            message.CC.Add("VCN430A@mazak.co.uk");
                         }
                         if (machType == "QT200/250" || machType == "QT300/350")
                         {
-                            message.CC.Add("jmoffat@mazak.co.uk");
+                            message.CC.Add("QT200@mazak.co.uk");
                         }
                         if (machType == "VTC800")
                         {
-                            message.CC.Add("rwellings@mazak.co.uk");
+                            message.CC.Add("VTC800@mazak.co.uk");
                         }
 
                         message.Subject += "Engineering";
@@ -60,14 +60,14 @@ namespace ConcessionMonitor
                     case 1:
                         //message.CC.Add("jturberfield@mazak.co.uk");
                         message.Subject = "Quality";
-                        message.CC.Add("cbills@mazak.co.uk");
+                        message.CC.Add("Quality@mazak.co.uk");
                         break;
 
                     //step 2 complete, send email to director
                     case 2:
                         //message.CC.Add("jturberfield@mazak.co.uk");
                         message.Subject = "Director";
-                        message.CC.Add("sastley@mazak.co.uk");
+                        message.CC.Add("Director@mazak.co.uk");
                         break;
 
                     //step 3 complete and concession rejected, send to machining
@@ -121,89 +121,67 @@ namespace ConcessionMonitor
                 case "Quality":
                     //message.CC.Add("jturberfield@mazak.co.uk");
                     message.Subject = section;                 
-                    message.CC.Add("cbills@mazak.co.uk");
-                    message.CC.Add("ghodgkins@mazak.co.uk");
-                    message.CC.Add("gbarnes@mazak.co.uk");
+                    message.CC.Add("Quality@mazak.co.uk");
                     break;
                 case "QT Lathe Group":
                     //message.CC.Add("jturberfield@mazak.co.uk");
                     message.Subject = section;
-                    message.CC.Add("dsimkin@mazak.co.uk");
-                    message.CC.Add("jmoffat@mazak.co.uk");
+                    message.CC.Add("QT@mazak.co.uk");
                     break;
                 case "VTC/VCN Commodity Group":
                     //message.CC.Add("jturberfield@mazak.co.uk");
                     message.Subject = section;
-                    message.CC.Add("atame@mazak.co.uk");
-                    message.CC.Add("tperrin@mazak.co.uk");
-                    message.CC.Add("alee@mazak.co.uk");
+                    message.CC.Add("VTC/VCN@mazak.co.uk");
                     break;
                 case "VTC800 Group":
                     //message.CC.Add("jturberfield@mazak.co.uk");
                     message.Subject = section;
-                    message.CC.Add("atame@mazak.co.uk");
-                    message.CC.Add("rwellings@mazak.co.uk");
-                    message.CC.Add("alee@mazak.co.uk");
+                    message.CC.Add("VTC800@mazak.co.uk");
                     break;
                 case "Logistics":
                     //message.CC.Add("jturberfield@mazak.co.uk");
                     message.Subject = section;
-                    message.CC.Add("kbuckley@mazak.co.uk");
-                    message.CC.Add("lwillems@mazak.co.uk");
+                    message.CC.Add("Logistics@mazak.co.uk");
                     break;
                 case "EDM":
                     message.Subject = section;
                     message.CC.Add("jturberfield@mazak.co.uk");                 
-                    message.CC.Add("kbuckley@mazak.co.uk");
-                    message.CC.Add("whenley@mazak.co.uk");
+                    message.CC.Add("EDM@mazak.co.uk");
                     break;
                 case "Machining":
                     //message.CC.Add("jturberfield@mazak.co.uk");
                     message.Subject = section;
-                    message.CC.Add("schapman@mazak.co.uk");
-                    message.CC.Add("dastley@mazak.co.uk");
-                    message.CC.Add("pjohnstone@mazak.co.uk");
-                    message.CC.Add("jmurphy@mazak.co.uk");
-                    message.CC.Add("mjacobs@mazak.co.uk");
+                    message.CC.Add("Machining@mazak.co.uk");
                     break;
                 case "SMD":
                     //message.CC.Add("jturberfield@mazak.co.uk");
                     message.Subject = section;
-                    message.CC.Add("wprice@mazak.co.uk");
-                    message.CC.Add("mscott@mazak.co.uk");
-                    message.CC.Add("nperry@mazak.co.uk");
-                    message.CC.Add("awatkins@mazak.co.uk");
+                    message.CC.Add("SMD@mazak.co.uk");
                     break;
                 case "Purchasing":
                     //message.CC.Add("jturberfield@mazak.co.uk");
                     message.Subject = section;
-                    message.CC.Add("medwards@mazak.co.uk");
-                    message.CC.Add("plawrence@mazak.co.uk");
+                    message.CC.Add("Purchasing@mazak.co.uk");
                     break;
                 case "General Assembly":
                     //message.CC.Add("jturberfield@mazak.co.uk");
                     message.Subject = section;
-                    message.CC.Add("ebrzezicki@mazak.co.uk");
-                    message.CC.Add("rhayes@mazak.co.uk");
+                    message.CC.Add("GeneralAssembly@mazak.co.uk");
                     break;
                 case "Elec/Modular/Unit":
                     //message.CC.Add("jturberfield@mazak.co.uk");
                     message.Subject = section;
-                    message.CC.Add("rlorenzosmith@mazak.co.uk");
-                    message.CC.Add("rhayes@mazak.co.uk");
+                    message.CC.Add("ElecModularUnit@mazak.co.uk");
                     break;
                 case "Powder Line":
                     //message.CC.Add("jturberfield@mazak.co.uk");
                     message.Subject = section;
-                    message.CC.Add("wprice@mazak.co.uk");
-                    message.CC.Add("cgrubb@mazak.co.uk");
+                    message.CC.Add("Powder@mazak.co.uk");
                     break;
                 case "Shipping":
                     //message.CC.Add("jturberfield@mazak.co.uk");
                     message.Subject = section;
-                    message.CC.Add("cbills@mazak.co.uk");
-                    message.CC.Add("ghodgkins@mazak.co.uk");
-                    message.CC.Add("bbryant@mazak.co.uk");
+                    message.CC.Add("Shipping@mazak.co.uk");
                     break;
             }
            
--- ConcessionMonitor/ConcessionMain.Designer.cs
+++ ConcessionMonitor/ConcessionMain.Designer.cs
@@ -48,9 +48,10 @@
             this.btnCreateFull.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCreateFull.BackColor = System.Drawing.Color.Transparent;
             this.btnCreateFull.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.btnCreateFull.Location = new System.Drawing.Point(1082, 56);
+            this.btnCreateFull.Location = new System.Drawing.Point(812, 46);
+            this.btnCreateFull.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCreateFull.Name = "btnCreateFull";
-            this.btnCreateFull.Size = new System.Drawing.Size(203, 38);
+            this.btnCreateFull.Size = new System.Drawing.Size(152, 31);
             this.btnCreateFull.TabIndex = 0;
             this.btnCreateFull.Text = "Create New Concession";
             this.btnCreateFull.UseVisualStyleBackColor = false;
@@ -58,9 +59,10 @@
             // 
             // txtConSearch
             // 
-            this.txtConSearch.Location = new System.Drawing.Point(80, 64);
+            this.txtConSearch.Location = new System.Drawing.Point(60, 52);
+            this.txtConSearch.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtConSearch.Name = "txtConSearch";
-            this.txtConSearch.Size = new System.Drawing.Size(246, 22);
+            this.txtConSearch.Size = new System.Drawing.Size(186, 20);
             this.txtConSearch.TabIndex = 2;
             this.txtConSearch.TextChanged += new System.EventHandler(this.txtConSearch_TextChanged_1);
             this.txtConSearch.MouseLeave += new System.EventHandler(this.txtConSearch_MouseLeave);
@@ -70,10 +72,11 @@
             // 
             this.lblUser.AutoSize = true;
             this.lblUser.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.lblUser.Location = new System.Drawing.Point(0, 737);
+            this.lblUser.Location = new System.Drawing.Point(0, 600);
+            this.lblUser.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblUser.Name = "lblUser";
-            this.lblUser.Padding = new System.Windows.Forms.Padding(5);
-            this.lblUser.Size = new System.Drawing.Size(85, 27);
+            this.lblUser.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lblUser.Size = new System.Drawing.Size(65, 21);
             this.lblUser.TabIndex = 3;
             this.lblUser.Text = "UserName";
             // 
@@ -85,12 +88,13 @@
             this.dgvLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.dgvLog.Location = new System.Drawing.Point(17, 100);
+            this.dgvLog.Location = new System.Drawing.Point(13, 81);
+            this.dgvLog.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dgvLog.Name = "dgvLog";
             this.dgvLog.RowHeadersWidth = 42;
             this.dgvLog.RowTemplate.Height = 24;
             this.dgvLog.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvLog.Size = new System.Drawing.Size(1268, 618);
+            this.dgvLog.Size = new System.Drawing.Size(951, 502);
             this.dgvLog.TabIndex = 5;
             this.dgvLog.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvLog_CellClick_1);
             this.dgvLog.ColumnHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvLog_ColumnHeaderMouseClick);
@@ -106,16 +110,18 @@
             this.panLog.Controls.Add(this.btnCreateFull);
             this.panLog.Controls.Add(this.txtConSearch);
             this.panLog.Location = new System.Drawing.Point(0, 0);
+            this.panLog.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panLog.Name = "panLog";
-            this.panLog.Size = new System.Drawing.Size(1298, 734);
+            this.panLog.Size = new System.Drawing.Size(974, 596);
             this.panLog.TabIndex = 6;
             // 
             // lblSearch
             // 
             this.lblSearch.AutoSize = true;
-            this.lblSearch.Location = new System.Drawing.Point(17, 64);
+            this.lblSearch.Location = new System.Drawing.Point(13, 52);
+            this.lblSearch.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblSearch.Name = "lblSearch";
-            this.lblSearch.Size = new System.Drawing.Size(57, 17);
+            this.lblSearch.Size = new System.Drawing.Size(44, 13);
             this.lblSearch.TabIndex = 7;
             this.lblSearch.Text = "Search:";
             // 
@@ -129,11 +135,12 @@
             // 
             // ConcessionMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1298, 764);
+            this.ClientSize = new System.Drawing.Size(974, 621);
             this.Controls.Add(this.panLog);
             this.Controls.Add(this.lblUser);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ConcessionMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Concession Monitor";
